7253a9c51b28bd6899a4402cc77b5e3e50089ddc
AdityaShelke29 <adityashelke2014@gmail.com>
2023-01-15 10:29:47 +0530

    initial commit

---
+++ react-task-tracker/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <div className = "App-Left" > </div>
+      <Header />
+      <div className = "App-Right" > </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-task-tracker/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css';
+import Button from './Button'
+
+const Header = () => {
+  return (
+    <div className = "App-Center">
+
+    </div>
+  )
+}
+
+export default Header
